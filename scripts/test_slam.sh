33154d9c2afb445cffe0d54669831c2d25806e49
Abdulrahman <amab1994@gmail.com>
2020-05-09 10:28:04 +0100

    packages
    packages

--- scripts/test_slam.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/amabz/catkin_ws/src/map/my_world.world " &
-sleep 5
-xterm -e " roslaunch turtlebot_gazebo gmapping_demo.launch " &
-sleep 5
-xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
-sleep 5
-xterm -e " roslaunch turtlebot_teleop keyboard_teleop.launch "
