3ad0e4cdeb377c759be3beb62ebfe589fc3a8e94
Abdulrahman <amab1994@gmail.com>
2020-05-09 10:17:59 +0100

    updated

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-export TURTLEBOT_GAZEBO_WORLD_FILE=~/catkin_home_robot/src/map/amabz.world
-
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch " &
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/amabz/catkin_ws/src/map/my_world.world " &
 sleep 5
 xterm -e " roslaunch turtlebot_gazebo gmapping_demo.launch " &
 sleep 5
-xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
 sleep 5
-xterm -e " roslaunch turtlebot_teleop keyboard_teleop.launch"
+xterm -e " roslaunch turtlebot_teleop keyboard_teleop.launch "
