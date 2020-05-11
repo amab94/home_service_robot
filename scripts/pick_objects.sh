877e0df6e5245ee3a2521f19d1347e2213adb7d3
Abdulrahman Albar <amab1994@gmail.com>
2020-05-11 05:58:25 +0100

    Added script for pick_object package

--- scripts/pick_objects.sh
+++ scripts/pick_objects.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+# Build the catkin_ws
+cd $(pwd)/../..; catkin_make
+
 xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/amabz/catkin_uws/src/map/my_world2.world " &
 sleep 5
 xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/amabz/catkin_uws/src/map/mymap.yaml " &
 sleep 5
-xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch "
+xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
 sleep 5
-xterm -e "rosrun pick_objects pick_objects_node"
+xterm -e "rosrun pick_objects pick_objects"
